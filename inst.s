3436a72209cb1c88b5e485ce9737f0f643609784
lattemps <juandiegopatinomunoz958@gmail.com>
2024-10-05 14:57:02 -0500

    reading file

--- inst.s
+++ inst.s
@@ -36,4 +36,15 @@ _start:
     movq    $0, %rdx
     movq    $8, %rax
     syscall
-    __fini  %r15
+    # Mapping file's content into memory by using mmap syscall.
+    # rax = mmap(NULL, r15 -> rsi, PROT_READ | PROT_WRITE, MAP_PRIVATE, edi -> r8, 0)
+    movq    %rdi, %r8
+    xorq    %rdi, %rdi
+    movq    %r15, %rsi
+    movq    $3, %rdx
+    movq    $2, %r10        # here
+    xorq    %r9, %r9
+    movq    $9, %rax
+    syscall
+
+    __fini  $69
