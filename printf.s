ceff4bb7347c72875ce03b05e0f80c90e8b5bd44
lattemps <juandiegopatinomunoz958@gmail.com>
2024-10-06 19:00:18 -0500

    printf: printing zero

--- printf.s
+++ printf.s
@@ -1,23 +1,36 @@
 .section    .rodata
-    .test:  .string "hola\n"
+    .nullstr_msg:   .string "[printf:fatal]: null string (rsi)\n"
+    .nullstr_len:   .quad   34
 
+    .buffovr_msg:   .string "[printf:fatal]: buffer overflow\n"
+    .buffovr_len:   .quad   32
+
+    .expectingfmt_msg:  .string "[printf:fatal]: format expected\n"
+    .expectingfmt_len:   .quad   32
+
+    .buffsz: .quad 1024
+    .test: .string "test %d\n"
 
 .section    .data
     .buffer:    .zero   1024
-
+    .numbuf:    .zero   32
 
 .section    .text
-.globl  _printf_
+.include    "macros.inc"
 
+.globl  _printf_
 
 # rdi: stream
 # rsi: fmt string
 _printf_:
+    cmpq    $0, %rsi
+    je      .fatal_null_str
     pushq   %rbp
     movq    %rsp, %rbp
     subq    $16, %rsp
     # -8(%rbp): stream to be written.............
     # -16(%rbp): number of bytes to be written...
+    # -24(%rbp): nth argument....................
     movq    %rdi, -8(%rbp)
     movq    $0, -16(%rbp)
     # r8: reads the fmt string...
@@ -25,7 +38,11 @@ _printf_:
     movq    %rsi, %r8
     leaq    .buffer(%rip), %r9
 ._pfloop:
-    # Making sure EOF has not been reached
+    # Making sure there is not overflow
+    movq    -16(%rbp), %rax
+    cmpq    %rax, .buffsz(%rip)
+    je      .fatal_buff_overflow
+    # Making sure there still is content to be read
     movzbl  (%r8), %eax
     testl   %eax, %eax
     jz      ._pffini
@@ -37,13 +54,39 @@ _printf_:
     movb    %al, (%r9)
     jmp     ._pfcontinue
 ._pfformat:
+    # r10 will hold this argument
+    movq    -24(%rbp), %rbx
+    leaq    16(%rbp), %r10
+    movq    (%r10, %rbx, 8), %r10
+    incq    %r8
+    movzbl  (%r8), %eax
+    cmpl    $'d', %eax
+    je      ._pf_fmt_num
+    cmpl    $'s', %eax
+    je      ._pf_fmt_str
+    cmpl    $'c', %eax
+    je      ._pf_fmt_chr
+    cmpl    $'%', %eax
+    je      ._pf_fmt_pct
+    jmp     .fatal_expecting_fmt
+._pf_fmt_num:
+    cmpq    $0, %r10
+    jne      ._pf_fmt_num_nz
+    movb    $'0', (%r9)
+    incq    -16(%rbp)
+    jmp     ._pfcontinue
+._pf_fmt_num_nz:
 
+
+
+._pf_fmt_str:
+._pf_fmt_chr:
+._pf_fmt_pct:
 
 ._pfcontinue:
     incq    %r8
     incq    %r9
     jmp     ._pfloop
-
 ._pffini:
     movq    -16(%rbp), %rdx
     leaq    .buffer(%rip), %rsi
@@ -53,9 +96,34 @@ _printf_:
     leave
     ret
 
+#  _____________
+# < Errors here >
+#  -------------
+#  \
+#   \
+#    \ >()_
+#       (__)__ _
+.fatal_null_str:
+    __eputs .nullstr_msg(%rip), .nullstr_len(%rip)
+    __fini  $1
+.fatal_buff_overflow:
+    __eputs .buffovr_msg(%rip), .buffovr_len(%rip)
+    __fini  $2
+.fatal_expecting_fmt:
+    __eputs .expectingfmt_msg(%rip), .expectingfmt_len(%rip)
+    __fini  $3
 
+
+#  _______________
+# < Tests here... >
+#  ---------------
+#  \
+#   \
+#    \ >()_
+#       (__)__ _
 .globl  _start
 _start:
+    pushq   $0
     movq    $1, %rdi
     leaq    .test(%rip), %rsi
     call    _printf_
